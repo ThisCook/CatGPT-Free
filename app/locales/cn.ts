bf49b40c91507acefe0873465a95edb0794b25e2
ThisCook <116243677+ThisCook@users.noreply.github.com>
2023-09-29 03:12:19 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -7,15 +7,16 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized: isApp
-      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      ? "访问密码不正确，请[点击输入](/#/auth)正确的访问密码。"
+      : "访问密码不正确，请[点击输入](/#/auth)正确的访问密码。",
   },
   Auth: {
-    Title: "需要密码",
-    Tips: "管理员开启了密码验证，请在下方填入访问码",
+    Title: "设置访问码",
+    Tips: "扫码关注公众号，发送“AI密码”获取"",
     Input: "在此处填写访问码",
     Confirm: "确认",
     Later: "稍后再说",
+    Qrcode: "https://gosh.im/qr/ad.jpg",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
